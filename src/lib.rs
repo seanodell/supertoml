592f78a2d7ebdbf377cfb2a3d001de7e12b4e1b5
Sean O'Dell <sean@celsoft.com>
2025-09-07 18:18:13 -0700

    Add support for outputting .tfvars files

--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,7 @@ pub mod plugins;
 mod resolver;
 
 pub use error::SuperTomlError;
-pub use formatter::{format_as_dotenv, format_as_exports, format_as_json, format_as_toml};
+pub use formatter::{
+    format_as_dotenv, format_as_exports, format_as_json, format_as_tfvars, format_as_toml,
+};
 pub use resolver::{resolve_table_recursive, Plugin, Resolver};
